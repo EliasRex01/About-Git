1168fc182d460a7de4d3ff86961460949400d8ef
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-14 12:30:34 -0400

    Update Crear ramas.py

--- Crear ramas.py
+++ Crear ramas.py
@@ -10,4 +10,24 @@ git branch nombre_rama
 -- forma 2
 git checkout -b nombre_rama
 
--- 
+-- Como crear ramas en git
+( No sé borra una rama sino que se hace es copiar una rama y trabajar sobre esa)
+git status (muestra si hay algo que commintear)
+git branch desarrollo
+git checkout -b desarrollo02
+
+La dif entre el branch y el checkout es que el segundo ya te lleva a la rama para trabajar y en el primero debes cambiar de rama.
+git status
+
+git add .
+git commit -am "añadido el fichero"
+git log --oneline
+
+Cambiar de rama
+git checkout master (llendo a la rama master)
+git checkout desarrollo (a la rama desarrollo)
+git status
+git add .
+git status
+
+git commit -am "archivo creado y modificado en la rama desarrollo"
