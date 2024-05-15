1a961db058db4274cf87322e5e89a3ee706f0f89
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-15 17:46:22 -0400

    Update Unir ramas.py

--- Unir ramas.py
+++ Unir ramas.py
@@ -1,1 +1,26 @@
 -- como unificar ramas
+
+-- se crea una rama
+git checkout -b prueba2
+
+git status
+git add .
+git commit -am "cambio desde prueba2"
+git log --oneline
+
+git checkout master
+git log --oneline
+
+-- Unificando las ramas 1 y 2
+
+-- ver listado de ramas
+git branch
+
+-- unificando 2 ramas
+git merge prueba1
+git log --oneline
+
+git merge prueba2
+git log --oneline
+
+-- se unieron a la rama principal
