d0931802fad2ce2f4ce475a077142bb458f5776e
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-16 00:24:21 -0400

    Create Archivo gitignore.py

---
+++ Archivo gitignore.py
@@ -0,0 +1,21 @@
+# cuando no quieres que un archivo o carpeta entre en el control de versiones se usa este archivo
+# registrando su nombre dentro del archivo
+
+# .gitignore (contenido abajo)
+.lib/
+temp/
+pass.*    # cualquiera de nombre pass no importa la extension
+clave.txt
+*.exe     # cualquier archivo de tipo exe
+/config       (muy usado al momento de hacer proyectos compartidos)
+
+# agregando el gitignore
+git status
+git add .gitignore
+
+# intentando agregar un archivo de gitignore
+git add clave.txt
+# no dejaria agregarlo y si se fuerza no tendria seguimiento del control de versiones
+
+# para ver la ayuda es con git help
+git help
