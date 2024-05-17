fd14c54483b08e5e4d776b089ec53724b71295dd
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-17 00:24:23 -0400

    Create Uso de fork.py

---
+++ Uso de fork.py
@@ -0,0 +1,5 @@
+# el fork se usa para dar feedback al codigo de otros usuarios de github
+
+# se puede hacer fork y luego clonar en el local para hacer modificaciones
+
+# posteriormente ya usando el send request se envia las modificaciones al repositorio del usuario para ver si las acepta
